7d16bd743b85fb089d24aed02e0bab46765acf1d
rodrigo-terra <rodrigo93terra@gmail.com>
2024-10-25 11:34:34 -0300

    Implementando efeito de rotate nos cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -14,15 +14,31 @@ const emojis = [
   "🥲",
   "🥲",
   "🤨",
-  "🤨"
+  "🤨",
 ];
 let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for(let i = 1; i < emojis.length; i++) {
+for(let i = 0; i < emojis.length; i++) {
   let box = document.createElement("div");
   box.className = "item";
   box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
   document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+  if(openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
+
+  if(openCards.length == 2) {
+    setTimeout(checkMatch , 500);
+  } 
+}
+
+function checkMatch() {
+  
 }
