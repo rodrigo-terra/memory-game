9be4b9894b746c0411e44dd31ea989861d9016bb
rodrigo-terra <rodrigo93terra@gmail.com>
2024-10-25 10:59:00 -0300

    Criação dos cards dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+  "😍",
+  "😍",
+  "😒",
+  "😒",
+  "😊",
+  "😊",
+  "😂",
+  "😂",
+  "😉",
+  "😉",
+  "😎",
+  "😎",
+  "🥲",
+  "🥲",
+  "🤨",
+  "🤨"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 1; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".game").appendChild(box);
+}
